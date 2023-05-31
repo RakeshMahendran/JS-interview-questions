2fe3356ecdee7c1a104f98b6367fb22bb116875a
Rakesh Mahendran <rakeshmahendran99@gmail.com>
2023-05-31 23:37:40 +0530

    Debouncing

---
+++ script.js
@@ -0,0 +1,12 @@
+//Debouncing and throttling in Javascript
+
+//What is debouncing?
+//Debouncing is simply setting up a delay before executing a function after an event occurs.
+//If the event is triggered again , then the delay will happen and then will the function be
+
+//Question No.1 
+//Create a button UI and add debounce as follows => 
+// => Show "Button pressed <X> tIMES" every time button is pressed 
+// => Increase "Triggered <Y> Times" count after 800ms of debounce
+
+0
